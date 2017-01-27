8ee52f49b54c1303483a2f4baf6b472654a09147
jeeve <jvjulien@free.fr>
2017-01-27 23:34:19 +0100

    ajout sommaire

---
+++ sommaire/sommaire.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="fr">
+   <head>
+      <title>en route...</title>
+      <META NAME="Description" CONTENT="Au travers de textes et de photos perso, ces pages sont une invitation au rêve. Sensible aux visages de la nature, je propose une petite balade en sa compagnie."/>
+	  <?php include("../includes/header.php"); ?>	
+	    
+	  <link href="css/rss.css" rel="stylesheet">
+      <link href="../css/fond-etoile.css" rel="stylesheet">
+      <style>
+         .hover_group:hover {
+         opacity: 1;
+         }
+         #projectsvg {
+         position: relative;
+         width: 100%;
+         padding-bottom: 77%;
+         vertical-align: middle;
+         margin: 0;
+         overflow: hidden;
+         }
+         #projectsvg svg {
+         display: inline-block;
+         position: absolute;
+         top: 0;
+         left: 0;
+         }
+		 
+		/* Sommaire */
+		.sommaire-item {
+		list-style-type: none;
+		margin-bottom: 15px;
+		}
+		
+      </style>
+
+   </head>
+   <body>
+      <div class="page-container">
+         <!-- top navbar -->
+	     <?php include("../includes/navbar.php"); ?>	  
+         <div class="container">
+            <div class="row row-offcanvas row-offcanvas-left">
+               <!-- sidebar -->
+			   <?php include("../includes/sidebar.php"); ?>		
+               <!-- main area -->
+               <div class="col-xs-12 col-sm-12 col-md-9 fond">
+                  <div class="col-xs-12 col-sm-9 fond">
+                     <h1>en route...</h1>
+                     <br>
+                     <figure id="projectsvg">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 417" preserveAspectRatio="xMinYMin meet">
+                           <image width="500" height="417" xlink:href="images/sommaire.jpg"></image>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../emotions/emotions.php">
+                                 <text x="219" y="243" font-size="20" style="fill:white">émotions</text>
+                                 <rect x="219" y="163" opacity="0.2" fill="#FFFFFF" width="108" height="80"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../mer/mer.php">
+                                 <text x="190" y="155" font-size="20" style="fill:white">parfum salé</text>
+                                 <rect x="190" y="67" opacity="0.2" fill="#FFFFFF" width="156" height="88"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../mer/rivages.php">
+                                 <text x="349" y="158" font-size="20" style="fill:white">rivages</text>
+                                 <rect x="349" y="92" opacity="0.2" fill="#FFFFFF" width="146" height="66"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../mer/saint-jacut.php">
+                                 <text x="332" y="242" font-size="20" style="fill:white">Saint-Jacut</text>
+                                 <rect x="332" y="161" opacity="0.2" fill="#FFFFFF" width="163" height="81"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../impressions/impressions.php">
+                                 <text x="105" y="245" font-size="20" style="fill:white">impressions</text>
+                                 <rect x="105" y="167" opacity="0.2" fill="#FFFFFF" width="110" height="78"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../reflexions/reflexions.php">
+                                 <text x="3" y="380" font-size="20" style="fill:white">?!?</text>
+                                 <rect x="3" y="320" opacity="0.2" fill="#FFFFFF" width="122" height="96"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../sensations/sensations.php">
+                                 <text x="0" y="136" font-size="20" style="fill:white">sensations</text>
+                                 <rect x="0" y="2" opacity="0.2" fill="#FFFFFF" width="120" height="134"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../astronomie/astronomie.php">
+                                 <text x="369" y="90" font-size="20" style="fill:white">vers les étoiles</text>
+                                 <rect x="369" y="2" opacity="0.2" fill="#FFFFFF" width="127" height="88"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../loire/loire.php">
+                                 <text x="126" y="380" font-size="20" style="fill:white">au fil de l'eau</text>
+                                 <rect x="126" y="320" opacity="0.2" fill="#FFFFFF" width="123" height="80"></rect>
+                              </a>
+                           </g>
+	                       <g class="hover_group" opacity="0">
+                              <a xlink:href="../loire/diapo.php">
+                                 <text x="126" y="310" font-size="20" style="fill:white">ainsi coule la Loire</text>
+                                 <rect x="126" y="230" opacity="0.2" fill="#FFFFFF" width="123" height="80"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../foret/foret.php">
+                                 <text x="252" y="380" font-size="20" style="fill:white">peuple végétal</text>
+                                 <rect x="252" y="320" opacity="0.2" fill="#FFFFFF" width="94" height="80"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../foret/diapo.php">
+                                 <text x="252" y="310" font-size="20" style="fill:white">rencontres végétales</text>
+                                 <rect x="252" y="230" opacity="0.2" fill="#FFFFFF" width="94" height="80"></rect>
+                              </a>
+                           </g>	
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../en-chemin/en-chemin.php">
+                                 <text x="349" y="380" font-size="20" style="fill:white">en chemin</text>
+                                 <rect x="349" y="320" opacity="0.2" fill="#FFFFFF" width="150" height="80"></rect>
+                              </a>
+                           </g>  
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../mer/diapo.php">
+                                 <text x="349" y="310" font-size="20" style="fill:white">lumières marines</text>
+                                 <rect x="349" y="230" opacity="0.2" fill="#FFFFFF" width="150" height="80"></rect>
+                              </a>
+                           </g>  						   
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../informatique/informatique.php">
+                                 <text x="2" y="313" font-size="20" style="fill:white">01101</text>
+                                 <rect x="2" y="200" opacity="0.2" fill="#FFFFFF" width="100" height="113"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../sensations/windsurf-kitesurf.php">
+                                 <text x="124" y="65" font-size="20" style="fill:white">windsurf / kitesurf</text>
+                                 <rect x="124" y="2" opacity="0.2" fill="#FFFFFF" width="108" height="63"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../sensations/powerkite.php">
+                                 <text x="127" y="157" font-size="20" style="fill:white">powerkite</text>
+                                 <rect x="127" y="73" opacity="0.2" fill="#FFFFFF" width="58" height="84"></rect>
+                              </a>
+                           </g>
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../a-propos/a-propos.php">
+                                 <text x="2" y="191" font-size="20" style="fill:white">à propos</text>
+                                 <rect x="2" y="140" opacity="0.2" fill="#FFFFFF" width="100" height="51"></rect>
+                              </a>
+                           </g>
+						  <!-- 
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../sensations/windsurf-kitesurf.php">
+                                 <text x="136" y="416" font-size="20" style="fill:white">windsurf / kite</text>
+                                 <rect x="136" y="388" opacity="0.2" fill="#FFFFFF" width="360" height="28"></rect>
+                              </a>
+                           </g>
+						  --> 
+                           <g class="hover_group" opacity="0">
+                              <a xlink:href="../science/science.php">
+                                 <text x="235" y="65" font-size="20" style="fill:white">e^(i.pi)+1=0</text>
+                                 <rect x="235" y="2" opacity="0.2" fill="#FFFFFF" width="129" height="63"></rect>
+                              </a>
+                           </g>
+                        </svg>
+                     </figure>
+					 
+		 <br><br>
+		 <div> 
+		 <div hidden><a id="page-precedente" href="../index.php">au gré du vent 1.0</a></div>
+		 <p align="right"><a id="page-suivante" href="../astronomie/astronomie.php">vers les étoiles</a></p>
+		 </div>
+		 
+				  <br><br>
+                  <div class="row">
+                     <div class="col-xs-1 col-sm-2 fond"></div>
+					  <div class="col-xs-10 col-sm-8 fond-table encadrement-table">				 
+            <div>
+			   <h2>Sommaire</h2>
+               <ul>
+				
+				<?php include("../includes/sommaire.php"); ?>					
+     		
+               </ul>
+            </div>	   	
+				   </div></div>			 
+		 
+				  <br><br>
+                  <div class="row">
+                     <div class="col-xs-1 col-sm-2 fond"></div>
+					  <div class="col-xs-10 col-sm-8 fond-table encadrement-table">				 
+            <div>
+			   <h2>Diaporamas</h2>
+               <ul>
+					<li class="sommaire-item"><a class="btn btn-primary" href="/mer/diapo.php">lumières marines</a></li>
+					<li class="sommaire-item"><a class="btn btn-primary" href="/loire/diapo.php">ainsi coule la Loire</a></li>
+					<li class="sommaire-item"><a class="btn btn-primary" href="/foret/diapo.php">rencontres végétales</a></li>
+               </ul>
+            </div>	   	
+				   </div></div>				 
+		 
+		 
+		 <br><br> 
+                  </div>
+				  
+
+				  
+                  <div class="col-xs-12 col-sm-3 fond">
+                     <br>
+					 <?php include("../includes/rss.php"); ?>  
+					 <!-- 
+                     <script type="text/javascript" src="http://output95.rssinclude.com/output?type=js&amp;id=560051&amp;hash=4304e0b5feb335410a5b2942d6b9de4d"></script>	
+					-->
+				  </div>
+               </div>
+               <!-- /.col-xs-12 main -->
+            </div>
+            <!--/.row-->
+         </div>
+         <!--/.container-->
+      </div>
+      <!--/.page-container-->
+	  <?php include("../includes/footer.php"); ?>
+   </body>
+</html>
