828b170eacf93e8cf14e867cbf2e0e10013093a9
jeeve <jvjulien@free.fr>
2017-02-22 23:26:24 +0100

    api

--- informatique/informatique.php
+++ informatique/informatique.php
@@ -57,8 +57,8 @@ Voici un exemple maison qui m'a permis de me faire la main avec cette techno. Ce
 Le code source est disponible sur <a href="https://github.com/jeeve/defautheque" target="_blank">Github</a>
 </p>
 <p>
-Pour cet exemple, afin d'établir la liaison entre l'application AngularJs cliente et la base de données MySql sur le serveur, j'ai utilisé l'api <a href="https://github.com/mevdschee/php-crud-api" target="_blank">php-crud-api</a> particulièrement efficace. Merci à leurs auteurs. 
- Celle-ci permet de créer un service Web de type REST afin de communiquer avec la base de données. Ainsi la requête GET <a href="http://defautheque.000webhostapp.com/rest/api.php/alliage" target="_blank">api.php/alliage</a> renvoye sous forme de tableau javascript les lignes de la table "alliage" de la base de données.
+Pour cet exemple, afin d'établir la liaison entre l'application cliente AngularJs et la base de données MySql sur le serveur, j'ai utilisé l'api <a href="https://github.com/mevdschee/php-crud-api" target="_blank">php-crud-api</a>. Simple et efficace, je remercie les auteurs. 
+ Celle-ci permet de créer un service Web de type REST afin de communiquer avec la base de données. Ainsi la requête GET <a href="http://defautheque.000webhostapp.com/rest/api.php/alliage" target="_blank">api.php/alliage</a> renvoie sous forme de tableau javascript les lignes de la table "alliage" de la base de données.
 </p>
 <p align="right"><em><font size="2">dimanche 12 février 2017</font></em></p>					
 
