f340a5363ae184f7d3c062559bc7c051e5f7c7ec
jeeve <jvjulien@free.fr>
2017-05-30 10:37:23 +0200

    beta

--- sensations/lac-lery-poses.php
+++ sensations/lac-lery-poses.php
@@ -189,7 +189,7 @@ if (!(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE ||
                         <div><p><label for="mon-heure2">et <span style="color:grey">(HH:MM)</span></label><input id="mon-heure2" type="text" name="heure2" value="17:00"></input></p></div>
                         <div><p><label for="mon-delta">toutes les</label><input id="mon-delta" type="text" name="delta" value="60"></input> minutes
                         <button type="button" onclick="getHistorique()">Afficher</button>
-						<button type="button" onclick="getTimelapse()">Créer timelapse</button>
+						<button type="button" onclick="getTimelapse()">Créer timelapse (beta)</button>
 						</p></div>
                 
                   </form>
