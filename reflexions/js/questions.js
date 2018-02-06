3f0a44c1e0c3c2c860d58ee7b17b3633d6a071a7
jeeve <jvjulien@free.fr>
2018-02-06 15:41:58 +0100

    bold

--- reflexions/js/questions.js
+++ reflexions/js/questions.js
@@ -16,7 +16,7 @@ function getQuestion() {
 				ligne = data.feed.entry[i];
 				nom = ligne.gsx$nompseudo.$t;
 				question = ligne.gsx$votrequestion.$t;
-				node.innerHTML = '<p>La question du jour de ' + nom + ' : ' + question + '<br><a href="https://docs.google.com/forms/d/e/1FAIpQLSdm50yNnhBLyAku2Z2QuQ6UY7OU2QA6JWtAk9lRhMI3NmUV0A/viewform?usp=sf_link" target="_blank">Posez votre question <span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a></p>';
+				node.innerHTML = '<p>La question du jour de ' + nom + ' : <b>' + question + '</b><br><a href="https://docs.google.com/forms/d/e/1FAIpQLSdm50yNnhBLyAku2Z2QuQ6UY7OU2QA6JWtAk9lRhMI3NmUV0A/viewform?usp=sf_link" target="_blank">Posez votre question <span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a></p>';
 			});
 }
 
