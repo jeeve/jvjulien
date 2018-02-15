60f1a8ee0d01e676a906d7262c83597eea69fa45
jeeve <jvjulien@free.fr>
2018-02-15 14:42:26 +0100

    permalien

--- reflexions/js/questions.js
+++ reflexions/js/questions.js
@@ -1,3 +1,28 @@
+/*
+function getParameterByName(name) {
+	name = name.replace(/[[]/, [).replace(/[]]/, ]);
+	var regexS = [?&] + name + =([^&#]*);
+	var regex = new RegExp(regexS);
+	var results = regex.exec(window.location.search);
+	if(results == null)
+		return ;
+	else
+		return decodeURIComponent(results[1].replace(/+/g, ));
+}
+*/
+
+$.urlParam = function(name){
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	if (results == null) {
+		return "null";
+	}
+	return results[1] || 0;
+}
+
+ function urlencode(text) {
+        return encodeURIComponent(text);
+ }
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
@@ -12,14 +37,34 @@ function getQuestion() {
 				var ligne;
 				var nom, question;
 				var node = document.getElementById("ligne-question");
-				var i = getRandomInt(data.feed.entry.length);
+				var i;
+				var paramQuestion = decodeURIComponent($.urlParam('question'));
+				if (paramQuestion == "null") {
+					i = getRandomInt(data.feed.entry.length);
+				}
+				else {
+					i = -1;
+					for (j=0; j < data.feed.entry.length; j++) {
+						if (data.feed.entry[j].gsx$votrequestion.$t == paramQuestion) {
+							i = j;	
+							break;
+						}
+					}
+				}
+				if (i == -1) {
+					i = getRandomInt(data.feed.entry.length);
+				}
 				ligne = data.feed.entry[i];
 				nom = ligne.gsx$nompseudo.$t;
 				if (nom == "") {
 					nom = "anonyme";
 				}
 				question = ligne.gsx$votrequestion.$t;
-				node.innerHTML = '<p><u>La question du jour de ' + nom + '</u> : <b>' + question + '</b><br><a href="https://docs.google.com/forms/d/e/1FAIpQLSfcCrNawvdiorWRtXlkL16VHePNTI47YPe8UmTwGDMVWOoQcQ/viewform?usp=pp_url&entry.1388707615=' + question + '&entry.664735575&entry.115939093" target="_blank">Votre réponse <span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>, <a href="https://docs.google.com/forms/d/e/1FAIpQLSdm50yNnhBLyAku2Z2QuQ6UY7OU2QA6JWtAk9lRhMI3NmUV0A/viewform?usp=sf_link" target="_blank">Posez une question <span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a></p>';
+				var paramPermalien = '';
+				if (paramQuestion == "null") {
+					paramPermalien = '&question=' + encodeURIComponent(question);
+				}
+				node.innerHTML = '<p><u>La question du jour de ' + nom + '</u> : <b>' + question + '</b><br><a href="https://docs.google.com/forms/d/e/1FAIpQLSfcCrNawvdiorWRtXlkL16VHePNTI47YPe8UmTwGDMVWOoQcQ/viewform?usp=pp_url&entry.1388707615=' + question + '&entry.664735575&entry.115939093" target="_blank">Votre réponse <span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>, <a href="https://docs.google.com/forms/d/e/1FAIpQLSdm50yNnhBLyAku2Z2QuQ6UY7OU2QA6JWtAk9lRhMI3NmUV0A/viewform?usp=sf_link" target="_blank">Posez une question <span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>, <a href="' + window.location + paramPermalien + '">Permalien</a></p>';
 				getReponses(question);
 			});
 }
