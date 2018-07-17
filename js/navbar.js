2b01aa691da04c695d1ae4f7a1c3121830b7e71c
jeeve <jvjulien@free.fr>
2018-07-17 17:17:56 +0200

    gras sous-menu

--- js/navbar.js
+++ js/navbar.js
@@ -15,5 +15,19 @@ $(document).ready(function() {
 					return false;
 				}				
 	}).addClass('active');
+	$('.active ul > li').filter(function() {
+		if ($(this).children('a').length != 0)
+				{
+					url = document.location.href; 
+					dir = url.substring(url.lastIndexOf('/')+1, url.length); // .../loire
+					//dir = dir.substring(dir.lastIndexOf('/') + 1); // loire
+					href = $(this).children('a')[0].getAttribute('href');
+					return href.indexOf(dir) > 0; 
+				}
+				else
+				{
+					return false;
+				}				
+	}).addClass('active2');	
 	$('.active ul').css('display', 'block');
 });
