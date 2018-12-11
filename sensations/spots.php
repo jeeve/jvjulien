c15a185f4782f4878e49c9673a9751ef17824784
jeeve <jvjulien@free.fr>
2018-12-11 21:53:53 +0100

    spots paris

--- sensations/spots.php
+++ sensations/spots.php
@@ -2,8 +2,14 @@
 <html lang="fr">
    <head>
       <title>Les spots d'Ile de France</title>
-      <META NAME="Description" CONTENT="Les spots de la région parisienne pour la plancheà voile et le windsurf."/>
+      <META NAME="Description" CONTENT="Les spots de la région parisienne pour la planche à voile et le windsurf."/>
 	  <?php include("../includes/header.php"); ?>	
+	  <style type="text/css">
+		.lien-collapse {
+			cursor: pointer;
+			text-decoration-line: underline;
+		}
+	  </style>
    </head>
    <body>
       <div class="page-container">
@@ -24,6 +30,7 @@
                <div class="row">
 					<div class="col-sm-8">
 						<div class="fond-table encadrement-table">
+
 						<script id="wg_fwdg_90210_3_1538749208510">
 (function (window, document) {
   var loader = function () {
@@ -36,7 +43,13 @@
   };
   window.addEventListener ? window.addEventListener("load", loader, false) : window.attachEvent("onload", loader);
 })(window, document);
-</script>						</div>		
+</script>						
+
+<!--
+						<script type="text/javascript" src="https://www.windfinder.com/widget/forecast/js/lac-de-vaires-sur-Marne?unit_wave=m&unit_rain=mm&unit_temperature=c&unit_wind=kts&days=4&show_day=1&show_pressure=0&show_waves=0"></script><noscript><a rel='nofollow' href='https://www.windfinder.com/forecast/lac-de-vaires-sur-Marne?utm_source=forecast&utm_medium=web&utm_campaign=homepageweather&utm_content=noscript-forecast'>Wind forecast for undefined</a> provided by <a rel='nofollow' href='https://www.windfinder.com?utm_source=forecast&utm_medium=web&utm_campaign=homepageweather&utm_content=noscript-logo'>windfinder.com</a></noscript>
+-->						
+
+						</div>		
 					</div>			
 				  <div class="visible-xs"><br></div>						
 				   <div class="col-sm-4">
@@ -45,8 +58,7 @@
 					</div>
 					<br>
 					    <div class="embed-responsive embed-responsive-4by3 ombre-image" style="background-image: none; background-color: black;">
-                          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26383.71131348098!2d2.7431159007940233!3d48.915702174750116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sfr!2sfr!4v1543606578279" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                        </div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d422235.08052076533!2d2.2754300925254305!3d48.904374951691736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!3m2!1d48.856614!2d2.3522219!4m5!1s0x47e61c3ceabfc0ff%3A0x40b82c3688c57b0!2sJablines!3m2!1d48.917429!2d2.761107!5e1!3m2!1sfr!2sfr!4v1544557168668" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>                        </div>
 
 				 </div>		
 			</div>
@@ -57,7 +69,24 @@
                <div class="row">
 					<div class="col-sm-8">
 						<div class="fond-table encadrement-table">
-						<script src="https://www.windguru.cz/js/widget.php?s=60276&m=3&uid=wg_fwdg_90210_3_1538749208510&wj=knots&tj=c&odh=0&doh=24&fhours=240&vt=forecasts&p=WINDSPD,GUST,MWINDSPD,SMER,TMPE,FLHGT,CDC,APCPs,RATING"></script>
+
+<script id="wg_fwdg_60276_3_1544556036427">
+(function (window, document) {
+  var loader = function () {
+    var arg = ["s=60276","m=3","uid=wg_fwdg_60276_3_1544556036427","wj=knots","tj=c","odh=0","doh=24","fhours=240","vt=forecasts",
+   "p=WINDSPD,GUST,MWINDSPD,SMER,TMPE,FLHGT,CDC,APCPs,RATING"];
+    var script = document.createElement("script");
+    var tag = document.getElementsByTagName("script")[0];
+    script.src = "https://www.windguru.cz/js/widget.php?"+(arg.join("&"));
+    tag.parentNode.insertBefore(script, tag);
+  };
+  window.addEventListener ? window.addEventListener("load", loader, false) : window.attachEvent("onload", loader);
+})(window, document);
+</script>			
+
+<!--
+						<script type="text/javascript" src="https://www.windfinder.com/widget/forecast/js/lac-de-vaires-sur-Marne?unit_wave=m&unit_rain=mm&unit_temperature=c&unit_wind=kts&days=4&show_day=1&show_pressure=0&show_waves=0"></script><noscript><a rel='nofollow' href='https://www.windfinder.com/forecast/lac-de-vaires-sur-Marne?utm_source=forecast&utm_medium=web&utm_campaign=homepageweather&utm_content=noscript-forecast'>Wind forecast for undefined</a> provided by <a rel='nofollow' href='https://www.windfinder.com?utm_source=forecast&utm_medium=web&utm_campaign=homepageweather&utm_content=noscript-logo'>windfinder.com</a></noscript>
+-->
 					</div>		
 				</div>			
 				  <div class="visible-xs"><br></div>						
@@ -67,15 +96,101 @@
 					</div>
 					<br>
 					    <div class="embed-responsive embed-responsive-4by3 ombre-image" style="background-image: none; background-color: black;">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m11!1m3!1d22212.469008144722!2d2.6236038765608063!3d48.85965918693715!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sfr!2sfr!4v1544546240272" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>  
-						  </div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d105647.94469813356!2d2.439899995693516!3d48.862143539942224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!3m2!1d48.856614!2d2.3522219!4m5!1s0x47e61074aa8587ab%3A0x40b82c3688c48d0!2sVaires-sur-Marne!3m2!1d48.873608999999995!2d2.6395429999999998!5e1!3m2!1sfr!2sfr!4v1544557341867" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				 </div>		
+			</div>		
+</div>
+			   <br><br>		   
+			    <h2>Moisson Lavacourt</h2>
+				<p>Orientations favorables : SO - NE</p>			                								
+               <div class="row">
+					<div class="col-sm-8">
+						<div class="fond-table encadrement-table">
+						
+<script id="wg_fwdg_581_3_1544556198330">
+(function (window, document) {
+  var loader = function () {
+    var arg = ["s=581","m=3","uid=wg_fwdg_581_3_1544556198330","wj=knots","tj=c","odh=0","doh=24","fhours=240","vt=forecasts",
+   "p=WINDSPD,GUST,MWINDSPD,SMER,TMPE,FLHGT,CDC,APCPs,RATING"];
+    var script = document.createElement("script");
+    var tag = document.getElementsByTagName("script")[0];
+    script.src = "https://www.windguru.cz/js/widget.php?"+(arg.join("&"));
+    tag.parentNode.insertBefore(script, tag);
+  };
+  window.addEventListener ? window.addEventListener("load", loader, false) : window.attachEvent("onload", loader);
+})(window, document);
+</script>				
+
+<!--
+						<script type="text/javascript" src="https://www.windfinder.com/widget/forecast/js/moisson_lavacourt?unit_wave=m&unit_rain=mm&unit_temperature=c&unit_wind=kts&columns=2&days=4&show_day=1&show_pressure=0&show_waves=0"></script><noscript><a rel='nofollow' href='https://www.windfinder.com/forecast/moisson_lavacourt?utm_source=forecast&utm_medium=web&utm_campaign=homepageweather&utm_content=noscript-forecast'>Wind forecast for Moisson Lavacourt</a> provided by <a rel='nofollow' href='https://www.windfinder.com?utm_source=forecast&utm_medium=web&utm_campaign=homepageweather&utm_content=noscript-logo'>windfinder.com</a></noscript>
+-->
+					</div>		
+				</div>			
+				  <div class="visible-xs"><br></div>						
+				   <div class="col-sm-4">
+				   <div class="embed-responsive ombre-image fond-table encadrement-table" style="height:170px;">
+  					<iframe src="meteo-temps-reel.php?station=64a25cf9-5156-425f-9706-e334dc0bc1d0" height=170></iframe>	
+					</div>
+					<br>
+					    <div class="embed-responsive embed-responsive-4by3 ombre-image" style="background-image: none; background-color: black;">
+<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d211164.66341434096!2d1.8751571688649815!3d48.89321926889306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!3m2!1d48.856614!2d2.3522219!4m5!1s0x47e6c12b153c22bf%3A0x2d227d4087bc4da9!2sMoisson!3m2!1d49.072928999999995!2d1.6691859999999998!5e1!3m2!1sfr!2sfr!4v1544557383138" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>						</div>
 
 				 </div>		
 			</div>
-			
+
 			
 <br>
 <br>
+
+			   <br><br>		   
+			    <h2>Léry-Poses</h2>			
+		<p><a data-toggle="collapse" data-target="#item-1" class="lien-collapse">Orientations</a></p>		
+							<a href="images/spots-lery-poses.jpg" target="_blank" id="item-1" class="collapse">
+							<img src="images/spots-lery-poses-2.jpg" width=300 class="img-responsive ombre-image" alt="Les mises à l'eau en fonction du vent" title="Les mises à l'eau en fonction du vent">
+							</a>
+							
+						
+				
+               <div class="row">
+					<div class="col-sm-8">
+						<div class="fond-table encadrement-table">
+
+<script id="wg_fwdg_4864_3_1544556530603">
+(function (window, document) {
+  var loader = function () {
+    var arg = ["s=4864","m=3","uid=wg_fwdg_4864_3_1544556530603","wj=knots","tj=c","odh=0","doh=24","fhours=240","vt=forecasts",
+   "p=WINDSPD,GUST,MWINDSPD,SMER,TMPE,FLHGT,CDC,APCPs,RATING"];
+    var script = document.createElement("script");
+    var tag = document.getElementsByTagName("script")[0];
+    script.src = "https://www.windguru.cz/js/widget.php?"+(arg.join("&"));
+    tag.parentNode.insertBefore(script, tag);
+  };
+  window.addEventListener ? window.addEventListener("load", loader, false) : window.attachEvent("onload", loader);
+})(window, document);
+</script>
+
+<!--
+						<script type="text/javascript" src="https://www.windfinder.com/widget/forecast/js/lake_des_deux_amants?unit_wave=m&unit_rain=mm&unit_temperature=c&unit_wind=kts&columns=2&days=4&show_day=1&show_pressure=0&show_waves=0"></script><noscript><a rel='nofollow' href='https://www.windfinder.com/forecast/lake_des_deux_amants?utm_source=forecast&utm_medium=web&utm_campaign=homepageweather&utm_content=noscript-forecast'>Wind forecast for Lac des deux Amants</a> provided by <a rel='nofollow' href='https://www.windfinder.com?utm_source=forecast&utm_medium=web&utm_campaign=homepageweather&utm_content=noscript-logo'>windfinder.com</a></noscript>
+-->						
+					</div>		
+				</div>			
+				  <div class="visible-xs"><br></div>						
+				   <div class="col-sm-4">
+				   <div class="embed-responsive ombre-image fond-table encadrement-table" style="height:170px;">
+  					<iframe src="meteo-temps-reel.php?station=33782285-df4e-4432-a3b3-06ddf1b3680a" height=170></iframe>	
+					</div>
+					<br>
+					    <div class="embed-responsive embed-responsive-4by3 ombre-image" style="background-image: none; background-color: black;">
+<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d421188.1259875271!2d1.5370175529156327!3d49.028173071130745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!3m2!1d48.856614!2d2.3522219!4m5!1s0x47e1289bac44658f%3A0xe940a5c757b0bf2a!2sL%C3%A9ry-Poses+en+Normandie%2C+Poses!3m2!1d49.302783!2d1.209404!5e1!3m2!1sfr!2sfr!4v1544558819114" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>				
+							</div>
+
+				 </div>		
+			</div>
+
+			
+<br>
+<br>
+
 
  				  <div id="swipe">
 					<div class="row">
